32917d55d23b14751102cf2c66badc97cc6776dc
Matt Boles <mboles@brightcove.com>
2013-02-22 11:41:58 -0500

    New Bootstrap CSS (combined) and menu issue correction

--- javascripts/views/topNav.js
+++ javascripts/views/topNav.js
@@ -1,21 +1,35 @@
 function topNavClickedBC( event ) {
   bc.device.navigateToView("brightcove.html");
+  if ($('#dmanualCollapse').hasClass('in')) {
+    $("#amanualCollapse").click();
+  }
 }
 
 function topNavClickedVC( event ) {
   bc.device.navigateToView("videocloud.html");
+  if ($('#dmanualCollapse').hasClass('in')) {
+    $("#amanualCollapse").click();
+  }
 }
-
 function topNavClickedAC( event ) {
   bc.device.navigateToView("appcloud.html");
+  if ($('#dmanualCollapse').hasClass('in')) {
+    $("#amanualCollapse").click();
+  }
 }
 
 function topNavClickedZC( event ) {
   bc.device.navigateToView("zencoder.html");
+  if ($('#dmanualCollapse').hasClass('in')) {
+    $("#amanualCollapse").click();
+  }
 }
 
 function topNavClickedS( event ) {
   bc.device.navigateToView("status.html");
+  if ($('#dmanualCollapse').hasClass('in')) {
+    $("#amanualCollapse").click();
+  }
 }
 
 Mark.pipes.date = function (date) {
